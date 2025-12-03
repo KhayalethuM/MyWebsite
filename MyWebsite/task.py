e3aad856e8b8ea7c26cdac319954f0672f6f5e9c
Tommorrow’s Echo <161151117+KhayalethuM@users.noreply.github.com>
2025-12-03 17:45:05 +0200

    Delete MyWebsite directory

--- MyWebsite/task.py
+++
@@ -1,6 +0,0 @@
-class Task:
-    def __init__(self, title): # Constructor runs whenever a Task is created
-        self.title = title # Stores the task title
-        self.done = False # Default: task not done
-    def mark_done(self): # Behavior (method) of the Task
-        self.done = True
